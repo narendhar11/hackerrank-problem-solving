be2d4fcdb3642f6e724e21ff7e2b318da3ab1618
Chiluka Narendhar <naru2112@gmail.com>
2022-04-29 17:20:38 +0530

    Update Plus_Minus.js

--- Plus_Minus.js
+++ Plus_Minus.js
@@ -1,5 +1,12 @@
 #Javascript
-arr = [-4, 3, -9, 0, 4, 1]
+arr = [1, 1, 0, -1, -1]
+There are n = 5 elements, two positive, two negative and one zero. Their ratios are
+2/5 = 0.400000, 2/5 = 0.400000 and 1/5 = 0.200000
+Answer should below
+0.400000
+0.400000
+0.200000
+
 var positiveElements = 0
 var negativeElements = 0
 var zeroElements = 0
